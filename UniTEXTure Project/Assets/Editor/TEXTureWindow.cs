3dff749a8d3dacbf0bb5d71f2e1ff165668dba81
Eli Samuel <49277886+eli-samuel@users.noreply.github.com>
2023-09-24 18:41:39 -0400

    Added important config fields and cleaned up the code a bit

--- UniTEXTure Project/Assets/Editor/TEXTureWindow.cs
+++ UniTEXTure Project/Assets/Editor/TEXTureWindow.cs
@@ -5,60 +5,71 @@ namespace Editor
 {
     public class TEXTureWindow : EditorWindow
     {
-        private string _prompt;
+        private string _prompt = "";
+        private string _expName = "";
+        private int _seed;
         private Texture2D _texture;
-        private static GUIStyle GenerateButton;
-        
+        private static GUIStyle GenerateButton = new GUIStyle("Button")
+        {
+            fontStyle = FontStyle.BoldAndItalic,
+            normal = { textColor = Color.gray },
+            hover = { textColor = Color.cyan },
+            active = { }
+        };
+
         [MenuItem("UniTEXTure/Create")]
         private static void ShowWindow()
         {
             var window = GetWindow<TEXTureWindow>();
             window.titleContent = new GUIContent("UniTEXTure Creator");
             window.minSize = new Vector2(400, 400);
-            // window.maxSize = new Vector2(400, 400);
             window.Show();
-            
-            GenerateButton = new GUIStyle("Button")
-            {
-                fontStyle = FontStyle.BoldAndItalic,
-                normal = {textColor = Color.gray},
-                hover = {textColor = Color.cyan}, 
-                active = {}
-            };
-
         }
-
-       
 
         private void OnGUI()
         {
+            var padding = 5;
             var itemRect = new Rect(20, 100, position.width-40, 18);
-            _prompt = EditorGUI.TextField(itemRect,
-                new GUIContent("Prompt", "Enter Prompt for Generating New Texture"), _prompt);
 
-            itemRect.y += itemRect.height + 5;
-            
-            _texture = (Texture2D)EditorGUI.ObjectField(itemRect,
-                new GUIContent("Texture (Optional)", "Enter Target Texture"), _texture, typeof(Texture2D), false);
-            
-            itemRect.y += itemRect.height + 5;
+            _prompt = DrawTextField(itemRect, "Prompt", "Enter Prompt for Generating New Texture", _prompt);
+            itemRect.y += itemRect.height + padding;
+
+            _expName = DrawTextField(itemRect, "Experiment Name", "Enter Experiment Name", _expName);
+            itemRect.y += itemRect.height + padding;
+
+            _seed = DrawIntField(itemRect, "Seed", "Enter Seed for Generating New Texture", _seed);
+            itemRect.y += itemRect.height + padding;
+
+            _texture = (Texture2D)DrawObjectField(itemRect, "Texture (Optional)", "Enter Initial Texture", _texture, typeof(Texture2D), false);
+            itemRect.y += itemRect.height + padding;
 
             var filePath = _texture ? Application.dataPath + "/" + AssetDatabase.GetAssetPath(_texture) : "No Texture Selected";
-            
-            EditorGUI.LabelField(itemRect, filePath, new GUIStyle(EditorStyles.boldLabel){});
-            
-            itemRect.y += itemRect.height + 5;
-            
+            EditorGUI.LabelField(itemRect, filePath, new GUIStyle(EditorStyles.boldLabel) { });
+            itemRect.y += itemRect.height + padding;
+
             EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(_prompt));
-            if(GUI.Button(itemRect, "Generate New Texture", GenerateButton)) Generate();
+            if (GUI.Button(itemRect, "Generate New Texture", GenerateButton)) Generate();
             EditorGUI.EndDisabledGroup();
+        }
+
+        private string DrawTextField(Rect rect, string label, string tip, string value)
+        {
+            return EditorGUI.TextField(rect, new GUIContent(label, tip), value);
+        }
+
+        private int DrawIntField(Rect rect, string label, string tip, int value)
+        {
+            return EditorGUI.IntField(rect, new GUIContent(label, tip), value);
+        }
+
+        private Object DrawObjectField(Rect rect, string label, string tip, Object value, System.Type type, bool allowSceneObjects)
+        {
+            return EditorGUI.ObjectField(rect, new GUIContent(label, tip), value, type, allowSceneObjects);
         }
 
         private void Generate()
         {
-            
-            Debug.Log("Generating New Texture");
-            
+            Debug.Log("Generating New Texture"); 
         }
     }
 }
