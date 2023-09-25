10203b830601f600e3dd09b7f554c8beb9bfb002
Eli Samuel <49277886+eli-samuel@users.noreply.github.com>
2023-09-24 23:15:13 -0400

    Added different style buttons to retrieve files and added a potential method to communicate with python files

--- UniTEXTure Project/Assets/Editor/TEXTureWindow.cs
+++ UniTEXTure Project/Assets/Editor/TEXTureWindow.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -44,14 +45,22 @@ namespace Editor
             _seed = DrawIntField(itemRect, "Seed", "Enter Seed for Generating New Texture", _seed);
             itemRect.y += itemRect.height + padding;
 
-            _texture = (Texture2D)DrawObjectField(itemRect, "Texture (Optional)", "Enter Initial Texture", _texture, typeof(Texture2D), false);
+            if (GUI.Button(itemRect, "Select Texture (Optional)"))
+            {
+                _texture = (Texture2D) SelectObject("Select Texture (Optional)", "png,jpg,jpeg");
+            }
             itemRect.y += itemRect.height + padding;
 
-            var filePath = _texture ? Application.dataPath + "/" + AssetDatabase.GetAssetPath(_texture) : "No Texture Selected";
-            EditorGUI.LabelField(itemRect, filePath, new GUIStyle(EditorStyles.boldLabel) { });
+            DisplayFilePath(itemRect, _texture);
             itemRect.y += itemRect.height + padding;
 
-            _objFile = DrawObjectField(itemRect, "3D Model", "Select .obj File", _objFile, typeof(Object), false);
+            if (GUI.Button(itemRect, "Select OBJ File"))
+            {
+                _objFile = SelectObject("Select OBJ File", "obj");
+            }
+            itemRect.y += itemRect.height + padding;
+
+            DisplayFilePath(itemRect, _objFile);
             itemRect.y += itemRect.height + padding;
 
             EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(_prompt) || string.IsNullOrEmpty(_expName) || _objFile == null || _seed < 0);
@@ -72,14 +81,101 @@ namespace Editor
             return EditorGUI.IntField(rect, new GUIContent(label, tip), value);
         }
 
+        // Currently an unused function (has been replaced with SelectObject(string, string))
         private Object DrawObjectField(Rect rect, string label, string tip, Object value, System.Type type, bool allowSceneObjects)
         {
             return EditorGUI.ObjectField(rect, new GUIContent(label, tip), value, type, allowSceneObjects);
         }
 
+        private Object SelectObject(string title, string type)
+        {
+            string path = EditorUtility.OpenFilePanel(title, "", type);
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                // Convert absolute path to relative path
+                string projectPath = Application.dataPath;
+                if (path.StartsWith(projectPath))
+                {
+                    // Make sure that the path starts with "Assets/"
+                    path = "Assets" + path.Substring(projectPath.Length);
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError("Selected texture is not within the project folder. Try again.");
+                }
+
+                return AssetDatabase.LoadAssetAtPath<Object>(path);
+            }
+
+            return null;
+        }
+
+        private void DisplayFilePath(Rect rect, Object obj)
+        {
+            string filePath = obj
+                ? $"{Application.dataPath}/{AssetDatabase.GetAssetPath(obj).Replace("Assets/", "")}"
+                : "No File Selected";
+            EditorGUI.LabelField(rect, filePath);
+        }
+
+
         private void Generate()
         {
-            Debug.Log("Generating New Texture"); 
+            UnityEngine.Debug.Log("Generating New Texture");
+
+            // Error Checking
+            if (_objFile == null || !IsObjFile(_objFile))
+            {
+                UnityEngine.Debug.LogError("Please select a valid .obj file.");
+                return;
+            }
+
+            string objFilePath = AssetDatabase.GetAssetPath(_objFile);
+
+            string pythonScriptPath = "C:\\Users\\bagli\\OneDrive\\Desktop\\test.py";
+
+            string command = $"python \"{pythonScriptPath}\" \"{_prompt}\" \"{_expName}\" {_seed} \"{objFilePath}\"";
+
+            // Start a process to execute the Python script
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+            };
+
+            Process process = new Process { StartInfo = psi };
+            process.Start();
+
+            // Pass the command to the command prompt
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+
+            // Read the output of the Python script
+            string output = process.StandardOutput.ReadToEnd();
+
+            // Close the process
+            process.WaitForExit();
+            process.Close();
+
+            // Log the Python script's output
+            UnityEngine.Debug.Log(output);
+        }
+
+        private bool IsObjFile(Object obj)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(obj);
+            if (string.IsNullOrEmpty(assetPath))
+            {
+                return false;
+            }
+
+            string extension = System.IO.Path.GetExtension(assetPath);
+            return extension.Equals(".obj", System.StringComparison.OrdinalIgnoreCase);
         }
     }
 }
