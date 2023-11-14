7e79d3f0355787573004a4751b4e731fd408eacb
Eli Samuel <49277886+eli-samuel@users.noreply.github.com>
2023-11-14 11:25:42 -0500

    Added python communication package file with socket passing

---
+++ PCP/csharp_out/ProtoTest/ProtoTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using Google.Protobuf;
+using stimulus; // Add this using directive
+
+namespace ProtoNamespace
+{
+    class ProtoTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            stimulus.MyMessage message = new stimulus.MyMessage // Specify the full namespace
+            {
+                Id = 1,
+                Name = "John"
+            };
+
+            byte[] bytes = message.ToByteArray();
+
+            // Assume Python server is running on localhost, port 5555
+            string pythonServerAddress = "127.0.0.1";
+            int pythonServerPort = 5555;
+
+            using (TcpClient client = new TcpClient(pythonServerAddress, pythonServerPort))
+            using (NetworkStream stream = client.GetStream())
+            {
+                // Send the serialized protobuf message to Python
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+
+    // You can keep the MyMessage class here, or move it to a separate file if needed
+    public partial class MyMessage : global::ProtoBuf.IExtensible
+    {
+        // ... other code
+    }
+}
